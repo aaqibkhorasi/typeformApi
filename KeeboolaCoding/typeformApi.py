d601f2e0ff9ef8e062984e05d54139c7c33d2f40
Aaqib Khorasi <akhorasi@sfu.ca>
2017-03-25 17:39:22 -0700

    Added Error handling and csv writer code

--- KeeboolaCoding/typeformApi.py
+++ KeeboolaCoding/typeformApi.py
@@ -1,30 +1,50 @@
 import requests
 import json
+import csv
+import sys
 
 TYPEFORM_KEY = "ac83034cfa742c0f79c26e9a612b4ba7e2aa0d3d"
 
 # TODO: Check for status codes for error handling
 # TODO: Use CSV library to produce data in CSV format
 def main():
-    resp = requests.get("https://api.typeform.com/v1/forms?key=" + TYPEFORM_KEY)
-    FORM_ID = resp.json()[0]['id']
-    resp = requests.get("https://api.typeform.com/v1/form/"+FORM_ID+"?key="+TYPEFORM_KEY)
-    with open('data.txt', 'w') as outfile:
-    	json.dump(resp.json(), outfile)
+    try:
+        resp = requests.get("https://api.typeform.com/v1/forms?key=" + TYPEFORM_KEY)
+        resp.raise_for_status()
+        FORM_ID = resp.json()[0]['id']
+    except requests.exceptions.HTTPError as err:
+        print(err)
+        sys.exit(1)
+
+    try:    
+        resp = requests.get("https://api.typeform.com/v1/form/"+FORM_ID+"?key="+TYPEFORM_KEY)
+        resp.raise_for_status()
+    except requests.exceptions.HTTPError as err:
+        print(err)
+        sys.exit(1)
     qIds = []
+    data = []
+    questions = []
+
+    writer = csv.writer(open('results.csv', 'w'), delimiter=',')
 
     for question in resp.json()["questions"]:
-        print(question["question"])
+        questions.append(question["question"])
         if question["id"] not in qIds:
             qIds.append(question["id"])
 
-    for response in resp.json()["responses"]:
+    writer.writerow(questions)
+    for i, response in enumerate(resp.json()["responses"]):
         answers = response["answers"]
+        data.append([])
         for qid in qIds:
             try:
-                print(answers[qid])
+                data[i].append(answers[qid])
             except:
-                print("NaN")
+                data[i].append("NaN")
+
+    writer.writerows(data)
 
 if __name__ == '__main__':
     main()
+            
